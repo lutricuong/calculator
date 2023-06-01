02d6dca3f313599483c25421a9a5d0094b7a315c
lutricuong <tcuong.lu@gmail.com>
2023-06-01 23:47:55 +0700

    clean code v1

--- javascript.js
+++ javascript.js
@@ -1,41 +1,14 @@
 const display = document.querySelector('.display');
 const result = document.querySelector('.result');
 const numAll = document.querySelectorAll('.num');
+const operation = document.querySelectorAll('.operation');
 const ac = document.querySelector('.ac');
 const c = document.querySelector('.c');
 const equal = document.querySelector('.equal');
+let arrNumAndOp;
 let num1;
 let num2;
 let op;
-
-ac.addEventListener("click", () => {
-    display.textContent = "";
-    result.textContent = 0;
-});
-c.addEventListener("click", () => {
-    display.textContent = display.textContent.slice(0,display.textContent.length -1);
-});
-numAll.forEach(num => {
-    num.addEventListener("click", () => {
-        if (num.textContent == '+' || num.textContent == '-' || num.textContent == 'x' || num.textContent == '/') {
-            display.textContent += " ";
-            display.textContent += num.textContent;
-            display.textContent += " ";
-        }
-        else display.textContent += num.textContent;
-        const arrNumAndOp = display.textContent.split(' ')
-        if (arrNumAndOp[2] == "") {
-            arrNumAndOp.pop();
-        }
-        console.log(arrNumAndOp);
-        equal.addEventListener("click", () => {
-            if (arrNumAndOp.length == 3) {
-                numberAndOp(arrNumAndOp);
-            }
-        })
-        
-    });
-});
 
 function numberAndOp(str) {
     num1 = +str[0];
@@ -46,3 +19,35 @@ function numberAndOp(str) {
     if (op == "x") return result.textContent = num1 * num2;
     if (op == "/") return result.textContent = num1 / num2;
 }
+
+ac.addEventListener('click', () => {
+    display.textContent = '';
+    result.textContent = 0;
+});
+c.addEventListener('click', () => {
+    display.textContent = display.textContent.slice(0,display.textContent.length -1);
+});
+equal.addEventListener('click', () => {
+    if (arrNumAndOp.length == 3) {
+        numberAndOp(arrNumAndOp);
+    }
+})
+
+numAll.forEach(num => {
+    num.addEventListener('click', () => {
+        display.textContent += num.textContent;
+        arrNumAndOp = display.textContent.split(' ');
+        if (arrNumAndOp[2] == '') {
+            arrNumAndOp.pop();
+        }
+        console.log(arrNumAndOp);
+    });
+});
+
+operation.forEach(oper => {
+    oper.addEventListener('click', () => {
+            display.textContent += ' ';
+            display.textContent += oper.textContent;
+            display.textContent += ' ';
+    })
+})
