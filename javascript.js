679c10bdaeb3e98a28b6108990da99f64f5253e3
lutricuong <tcuong.lu@gmail.com>
2023-06-04 16:36:38 +0700

    fix bug

--- javascript.js
+++ javascript.js
@@ -21,21 +21,29 @@ function numberAndOp(str) {
     if (op == "x") return result.textContent = num1 * num2;
     if (op == "/") return result.textContent = num1 / num2;
 }
-
 function addAndRemoveTransition(e) {
     e.classList.add('play');
     e.addEventListener('transitionend', () => {
         e.classList.remove('play')
     });
 }
+
 ac.addEventListener('click', () => {
     addAndRemoveTransition(ac);
-    display.textContent = '';
-    result.textContent = 0;
+    display.textContent = 0;
+    result.textContent = '';
 });
 c.addEventListener('click', () => {
     addAndRemoveTransition(c);
-    display.textContent = display.textContent.slice(0,display.textContent.length -1);
+    if (result.textContent != '') {
+        display.textContent = 0;
+        result.textContent = '';
+    }
+    else if (display.textContent.slice(display.textContent.length-1,display.textContent.length) == ' ') {
+        display.textContent = display.textContent.slice(0,display.textContent.length -3);
+    }
+    else     display.textContent = display.textContent.slice(0,display.textContent.length -1);
+    
 });
 equal.addEventListener('click', () => {
     addAndRemoveTransition(equal);
@@ -45,7 +53,11 @@ equal.addEventListener('click', () => {
 numAll.forEach(num => {
     num.addEventListener('click', () => {
         addAndRemoveTransition(num);
-        display.textContent += num.textContent;
+        if (display.textContent == 0) {
+            display.textContent = '';
+            display.textContent += num.textContent;
+        }
+        else display.textContent += num.textContent;
         arrNumAndOp = display.textContent.split(' ');
         if (arrNumAndOp[2] == '') {
             arrNumAndOp.pop();
@@ -56,19 +68,29 @@ numAll.forEach(num => {
 
 operation.forEach(oper => {
     oper.addEventListener('click', () => {
-            addAndRemoveTransition(oper);
+        addAndRemoveTransition(oper);
+        if (oper.textContent == '-' && display.textContent == '') {
+            display.textContent = '-';
+        }
+        else if (oper.textContent == '+' && display.textContent == ''
+            || oper.textContent == 'x' && display.textContent == ''
+            || oper.textContent == '/' && display.textContent == '') {
+            display.textContent = '';
+        }
+        else {
             display.textContent += ' ';
             display.textContent += oper.textContent;
             display.textContent += ' ';
-            arrNumAndOp = display.textContent.split(' ');
-            if (arrNumAndOp[2] == '') {
-                arrNumAndOp.pop();
-            }
-            console.log(arrNumAndOp);
-            if (arrNumAndOp.length > 3) {
-                numberAndOp(arrNumAndOp);
-                display.textContent = result.textContent + ' ' + oper.textContent + ' ';
-            }
+        }
+        arrNumAndOp = display.textContent.split(' ');
+        if (arrNumAndOp[2] == '') {
+            arrNumAndOp.pop();
+        }
+        console.log(arrNumAndOp);
+        if (arrNumAndOp.length > 3) {
+            numberAndOp(arrNumAndOp);
+            display.textContent = result.textContent + ' ' + oper.textContent + ' ';
+        }
     })
 });
 
